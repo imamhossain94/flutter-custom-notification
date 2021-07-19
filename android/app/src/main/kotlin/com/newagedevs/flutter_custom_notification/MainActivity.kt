998740126363b68e20606e3edc765b7e9ee646f5
imamhossain94 <imamagun94@gmail.com>
2021-07-19 18:58:30 +0600

    feat: first native MethodChannel added.

--- android/app/src/main/kotlin/com/newagedevs/flutter_custom_notification/MainActivity.kt
+++ android/app/src/main/kotlin/com/newagedevs/flutter_custom_notification/MainActivity.kt
@@ -1,6 +1,62 @@
 package com.newagedevs.flutter_custom_notification
 
+import android.os.Bundle
+import androidx.annotation.NonNull
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodChannel
+import io.flutter.plugins.GeneratedPluginRegistrant
 
 class MainActivity: FlutterActivity() {
+    private val CHANNEL = "flutter.native/helper"
+
+//    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+//        super.configureFlutterEngine(flutterEngine)
+//        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL).setMethodCallHandler {
+//            call, result ->
+//            // Note: this method is invoked on the main thread.
+//            // TODO
+//        }
+//    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        GeneratedPluginRegistrant.registerWith(FlutterEngine(this))
+
+        MethodChannel(flutterEngine!!.dartExecutor.binaryMessenger, CHANNEL).setMethodCallHandler { call, result ->
+//            if (call.method == "getBatteryLevel") {
+//                val batteryLevel = getBatteryLevel()
+//                if (batteryLevel != -1) {
+//                    result.success(batteryLevel)
+//                } else {
+//                    result.error("UNAVAILABLE", "Battery level not available.", null)
+//                }
+//            } else {
+//                result.notImplemented()
+//            }
+
+            if (call.method.equals("helloFromNativeCode")) {
+                val greetings = helloFromNativeCode()
+                result.success(greetings)
+            }
+
+        }
+    }
+
+//    private fun getBatteryLevel(): Int {
+//        val batteryLevel: Int
+//        if (VERSION.SDK_INT >= VERSION_CODES.LOLLIPOP) {
+//            val batteryManager = getSystemService(Context.BATTERY_SERVICE) as BatteryManager
+//            batteryLevel = batteryManager.getIntProperty(BatteryManager.BATTERY_PROPERTY_CAPACITY)
+//        } else {
+//            val intent = ContextWrapper(applicationContext).registerReceiver(null, IntentFilter(Intent.ACTION_BATTERY_CHANGED))
+//            batteryLevel = intent!!.getIntExtra(BatteryManager.EXTRA_LEVEL, -1) * 100 / intent.getIntExtra(BatteryManager.EXTRA_SCALE, -1)
+//        }
+//        return batteryLevel
+//    }
+
+    private fun helloFromNativeCode(): String {
+        return "Hello from Native Android Code"
+    }
+
 }
